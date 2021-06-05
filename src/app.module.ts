f1bcb64c791a0581ded165446c9119037dc6c2fe
kkoon9 <rndrnjs2003@gmail.com>
2021-06-05 16:56:39 +0900

    feat: User Schema 정의 - decorator version
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ProfileController } from './profile/profile.controller';
 import { ProfileModule } from './profile/profile.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,7 +8,6 @@ import { PostModule } from './post/post.module';
 @Module({
   imports: [
     ProfileModule,
-    MongooseModule.forRoot('MONGODB_URL'),
     AuthModule,
     UserModule,
     PostModule,

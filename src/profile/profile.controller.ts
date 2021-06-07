29641a3b09a6dfb7f91451eca6573c4ad48400f0
kkoon9 <rndrnjs2003@gmail.com>
2021-06-07 23:49:58 +0900

    feat: API 목록 정리
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 
 @Controller('api/profile')
 export class ProfileController {
+	private logger = new Logger('ProfileController');
+
 	constructor(private profileService : ProfileService) {}
 
-		@Get()
-		getAllProfiles() {
-			return this.profileService.getAllProfiles();
-		}
+	/**
+	 *  @route GET api/profile
+	 *  @desc Get all profiles
+	 *  @access Public
+	 */
+
+	/**
+	 *  @route GET api/profile/user/:user_id
+	 *  @desc Get profile by user ID
+	 *  @access Public
+	 */
+
+	/**
+	 *  @route GET api/profile/github/:username
+	 *  @desc Get user repos from github
+	 *  @access Public
+	 */
+
+	/**
+	 *  @route GET api/profile/me
+	 *  @desc Get current users profile
+	 *  @access Private
+	 */
+
+	/**
+	 *  @route POST api/profile
+	 *  @desc Create or update user profile
+	 *  @access Private
+	 */
+
+	/**
+	 *  @route PUT api/profile/experience
+	 *  @desc Add profile experience
+	 *  @access Private
+	 */
+
+	/**
+	 *  @route PUT api/profile/education
+	 *  @desc Add profile education
+	 *  @access Private
+	 */
 }
